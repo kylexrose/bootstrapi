45a558bdff813f32f5516f396bea9258630a7bf0
kylexrose@gmail.com <kylexrose@gmail.com>
2021-04-08 11:48:17 -0400

    adds btc tracker

--- main.js
+++ main.js
@@ -35,3 +35,15 @@ document.querySelector("#searchWeather").addEventListener('click', () =>{
 
         })
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btcURL = 'https://api.coinlore.net/api/ticker/?id=90';
+    fetch (btcURL)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            let price = data[0].price_usd;
+            formattedPrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            document.querySelector('#bitcoin p').innerHTML += `$${formattedPrice}`;
+        })
+})
